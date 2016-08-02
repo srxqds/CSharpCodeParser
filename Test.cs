99b3fbe688693ef757152aebddaa573afd5dd4b0
srxqds <gd.s.qiu@gmail.com>
2016-08-02 22:03:23 +0800

    【NEW】add test example

---
+++ Test.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using CSharpCodeParser;
+using System.IO;
+using UnityEngine;
+
+public class Test 
+{
+	[MenuItem("Test/CsParser")]
+	public static void DoTest()
+	{
+		CsTextBuffer tb = new CsTextBuffer ();
+		string cs = File.ReadAllText (Application.dataPath + "/Plugins/Editor/Test1.cs");
+		tb.LoadCsCode (cs);
+		Debug.LogError (tb);
+		CsGrammar csGrammar = new CsGrammar ();
+		Scanner scanner = new Scanner (csGrammar, tb.formatedLines, "test");
+		ParseTree parseTree = csGrammar.parser.ParseAll (scanner);
+		Debug.LogError (parseTree);
+	}
+
+	[MenuItem("Test/DoTest1")]
+	public static void DoTest1()
+	{
+		Debug.Log(CsParser.IsWhitespace ('\t'));
+
+		CsGrammar csGrammar = new CsGrammar ();
+		Debug.LogError (csGrammar.parser);
+	}
+}
---
+++ Test1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+
+public class Test1<T>
+{
+
+	public T gen;
+	public int field;
+	public int GetValue()
+	{
+		return 0;
+	}
+
+	[UnityEditor.MenuItem("Test")]
+	public static void AttributeTest()
+	{
+	}
+}
